868793f758641d449b8fd17f4fdedfea6cf2fb3c
Nailson Ferreira <nailson_ferreira@hotmail.com>
2019-07-06 22:50:09 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.Loja/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alura.Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
